af69481e350440d12a9a741dbed927eecd7d2302
Cheolho Jang <CJang@wildcatdiscovery.com>
2020-09-08 10:46:36 -0700

    graph seems to be working now...

--- utils/webapp.py
+++ utils/webapp.py
@@ -5,7 +5,6 @@ from matplotlib.figure import Figure
 from tools import *
 import os
 import io
-
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './uploads/'
 app.config['ALLOWED_EXTENSIONS'] = set(['mpt'])
@@ -48,19 +47,27 @@ def upload():
 def display_mpt(mpt):
    mpt = mpt_data(r"C:\Users\cjang.WILDCAT\Desktop\eis\eis_manager\data\\", [mpt])
    df = mpt.df_raw[['f', 're', 'im']]
-   plot(df['re'], df['im'])
-   return render_template('dataframe_view.html', data = df.to_html(), df_head = (mpt.data))
+   #plot(df['re'], df['im'])
+   return plot(df['re'], df['im'], mpt.data[0])
+   #return render_template('dataframe_view.html', data = df.to_html(), df_head = (mpt.data))
 
-def plot(xs, ys):
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-    axis.plot(xs, ys)
-    canvas = FigureCanvas(fig)
-    output = StringIO.StringIO()
-    canvas.print_png(output)
-    response = make_response(output.getvalue())
-    response.mimetype = 'image/png'
-    return response
+def plot(xs, ys, title):
+
+   fig = Figure(dpi=120, figsize = [15, 25], facecolor='w', edgecolor='w')
+   fig.subplots_adjust(left=0.1, right=0.95, hspace=0.5, bottom=0.1, top=0.95)
+   ax = fig.add_subplot(211, aspect='equal')
+   ax.plot(xs, ys, marker='o', ms=4, lw=2, ls='-', label = "Nvyquist Impedance Data")
+   ax.legend()
+   ax.set_title(title)
+    #fig = Figure()
+    #axis = fig.add_subplot(1, 1, 1)
+    #axis.plot(xs, ys, '.')
+   canvas = FigureCanvas(fig)
+   output = io.BytesIO()
+   canvas.print_png(output)
+   response = make_response(output.getvalue())
+   response.mimetype = 'image/png'
+   return response
 
 if __name__ == '__main__':
    app.secret_key = 'asdw34gegasdgf'
